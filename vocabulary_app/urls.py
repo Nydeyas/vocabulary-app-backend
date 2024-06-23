1c5873900bed13826df4c549ded3f967f8fe163f
Arkadiusz Polec <163996@stud.prz.edu.pl>
2024-06-23 18:23:15 +0200

    Set /api as default home view with default redirection to /api URL

--- vocabulary_app/urls.py
+++ vocabulary_app/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 
 from django.urls import path
+from django.views.generic.base import RedirectView
 from website.views import *
 
 urlpatterns = [
-    path('', HomeView.as_view()),
+    path('api', HomeView.as_view()),
     path('api/login', LoginView.as_view()),
     path('api/register', RegisterView.as_view()),
     path('api/user', UserView.as_view()),
@@ -28,4 +29,7 @@ urlpatterns = [
     path('api/user/<int:user_id>', UserDetailApiView.as_view()),
     path('api/category/<int:category_id>', CategoryDetailApiView.as_view()),
     path('api/word/<int:word_id>', WordDetailApiView.as_view()),
+
+    # Redirect to HomeView
+    path('', RedirectView.as_view(url='/api'), name='api-redirect'),
 ]
